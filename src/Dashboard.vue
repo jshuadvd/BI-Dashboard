ef5b70532df3fb1d7ef5792664f500b5551bc822
阿拉猪 <zrcccrz@outlook.com>
2020-11-17 21:14:23 +0800

    feat: 报表标题和按钮

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -4,12 +4,50 @@
       <sidenav @add-item="addItem" />
     </div>
 
-    <div class="bi-canvas">
-      <v-toolbar class="bi-app-bar" dense>
-        <v-toolbar-title>Application</v-toolbar-title>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      </v-toolbar>
+    <v-toolbar class="bi-app-bar" dense>
+      <v-toolbar-title
+        class="app-bar-title"
+      >
+        <input
+          v-if = "edited"
+          v-model = "title"
+          @blur= "edited = false; $emit('update')"
+          @keyup.enter = "edited=false; $emit('update')"
+        />
+        <div v-else>
+          <label @click = "edited = true;"> {{title}} </label>
+        </div>
+      </v-toolbar-title>
+      <div>
+        <v-btn-toggle
+          v-model="dashStatus"
+          mandatory
+          color="blue darken-2"
+        >
+          <v-btn text medium outlined>
+            编辑
+          </v-btn>
 
+          <v-btn text medium outlined>
+            预览
+          </v-btn>
+        </v-btn-toggle>
+
+        <div class="bi-btn">
+          <v-btn text medium outlined>
+            保存
+          </v-btn>
+        </div>
+        <div class="bi-btn">
+          <v-btn text medium outlined>
+            返回继续浏览
+          </v-btn>
+        </div>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </div>
+    </v-toolbar>
+
+    <div class="bi-canvas">
       <!-- main -->
       <Canvas :layout="layout" @del-item="delItem" />
     </div>
@@ -42,6 +80,12 @@ export default {
       },
     ],
     index: 1,
+    // 报名的名字
+    title: '未命名报表',
+    // 是否在编辑状态
+    edited: false,
+    // 编辑还是保存
+    dashStatus: 0,
   }),
 
   methods: {
@@ -58,6 +102,9 @@ export default {
     delItem(ind) {
       this.layout = this.layout.filter((d) => d.i !== ind);
     },
+    editTodo(todo) {
+      this.edited = todo;
+    },
   },
 };
 </script>
@@ -71,6 +118,43 @@ export default {
     .v-toolbar__content {
       justify-content: space-between;
     }
+
+    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2)!important;
+
+    .bi-btn {
+      border-radius: 4px;
+      display: inline-flex;
+      margin: 0 1px;
+
+      .v-btn {
+        height: 48px;
+        min-height: 0;
+        min-width: 48px;
+      }
+    }
+
+    .app-bar-title {
+      margin-left: -5px;
+
+      label, input {
+        user-select: none;
+      }
+
+      input {
+        border: 1px solid #ccc;
+        background: transparent;
+        padding: 2px 7px;
+
+        &:focus {
+          outline: none;
+        }
+      }
+
+      div {
+        padding: 2px 7px;
+        border: 1px solid #fff;
+      }
+    }
   }
 
   .bi-main {
@@ -80,17 +164,13 @@ export default {
     .bi-sidenav {
       position: absolute;
       right: 0;
-      width: 15vw;
       bottom: 0;
+      top: 48px;
+      width: 275px;
+      background: #fff;
       transform: translate3d(0px, 0px, 0px);
-      position: absolute;
-      width: 280px;
-      background: #fefefe;
-      height: 100%;
       transition: transform .3s ease-in-out;
-      box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
-        0px 4px 5px 0px rgba(0, 0, 0, 0.14),
-        0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+      box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2);
     }
 
     .bi-canvas {
@@ -100,12 +180,12 @@ export default {
   }
 
   .hide-drawer {
-      .bi-canvas {
-        margin-right: 0;
-      }
-
-      .bi-sidenav {
-        transform: translate3d(280px, 0px, 0px);
-      }
+    .bi-canvas {
+      margin-right: 0;
     }
+
+    .bi-sidenav {
+      transform: translate3d(280px, 0px, 0px);
+    }
+  }
 </style>
