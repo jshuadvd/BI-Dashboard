6afa1d9505cd52003755eaf5b54af52cb8b128f7
阿拉猪 <zrcccrz@outlook.com>
2020-11-18 21:44:02 +0800

    change: 将d3改成v5版本, 修改vue.config

--- vue.config.js
+++ vue.config.js
@@ -41,25 +41,5 @@ module.exports = {
     },
   },
 
-  // module: {
-  //   rules: [
-  //     // SCSS与SASS相比有不同的行尾
-  //     // 并且在import后面需要分号
-  //     {
-  //       test: /\.scss$/,
-  //       use: [
-  //         'vue-style-loader',
-  //         'css-loader',
-  //         {
-  //           loader: 'sass-loader',
-  //           // 需要 sass-loader@^8.0.0
-  //           options: {
-  //             // 这是您的变量文件的路径
-  //             prependData: "@import '@/styles/variables.scss';",
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
+  productionSourceMap: false,
 };
